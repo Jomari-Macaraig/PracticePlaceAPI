a5a3d2a1b9fe5a11b1608dd163a0ed5820955af3
Jomari Macaraig <macaraig.jomari10@gmail.com>
2018-09-27 15:23:33 +0000

    Created utility generator function to get places details

--- app/place/utils.py
+++ app/place/utils.py
@@ -11,3 +11,23 @@ def find_places_ids(query):
     results = resp['results']
     places_ids = [place['place_id'] for place in results]
     return places_ids
+
+def get_places_details(places_ids=None):
+    FIELDS = [
+        'name',
+        'website',
+        'formatted_address',
+        'formatted_phone_number'
+    ]
+    places_ids = places_ids or []
+    for place_id in places_ids:
+        result = client.place(
+            place_id=place_id,
+            fields=FIELDS
+        )['result']
+        yield {
+            'name': result['name'],
+            'website': result['website'],
+            'formatted_address': result['formatted_address'],
+            'formatted_phone_number': result['formatted_phone_number']
+        }
